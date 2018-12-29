4b8eb449da0663156bc4ad9550e991433056fe97
Matheus Gusmão <matheus.lemos@ccc.ufcg.edu.br>
2018-12-29 15:23:52 -0200

    Adicionando aula sobre Set e Booleano.

---
+++ Conceitos básicos de estrutura de dados e objetos em Python/Sets e Booleanos/set_boolean.py
@@ -0,0 +1,33 @@
+# Projeto em python 3 referente a aula 20 do Curso python da plataforma Udemy.
+
+# Lista
+
+lista = [1,2,1,2,34,521,4121,51515]
+
+# Criando um set
+
+numeros = set()
+
+# Adicionando valores ao set
+
+numeros.add(1)
+numeros.add(2)
+numeros.add(3)
+numeros.add(2)
+numeros.add(4)
+
+# Exibindo os valores armazenados no set, não há valores repetidos, pois set é uma estrutura que não aceita repetições
+print(numeros)
+# Exibindo os valores armazenados no set
+print(set(lista))
+
+# Booleanos
+
+# True para verdadeiro
+# False para falso
+# None como um espaço reservado para um objeto que não queremos reatribuir ainda
+
+# resultado 'False'
+print(1>2)
+# resultado 'True'
+print(4<=4)
