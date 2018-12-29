6de5417ce138e0ae7ad4d525b49c0399a8b3b0ef
Matheus Gusmão <matheus.lemos@ccc.ufcg.edu.br>
2018-12-29 16:10:22 -0200

    Adicionando aula sobre Operadores de comparação.

---
+++ operadores_de_comparacao.py
@@ -0,0 +1,57 @@
+# Projeto em python 3 referente a aula 24 do Curso python da plataforma Udemy.
+
+# Operadores
+
+# == : igual
+# != : diferente
+# <> : diferentes
+# < : menor que
+# > : maior que
+# <= : menor ou igual
+# >= : maior ou igual
+
+# Igualdade '=='
+
+# resultado 'True'
+print(2==2)
+# resultado 'False'
+print(1==0)
+
+# Diferença '!=,<>''
+
+# resultado 'True'
+print(2!=1)
+# resultado 'False'
+print(2!=2)
+# resultado 'True'
+print(2 <> 1)
+# resultado 'False'
+print(2 <> 2)
+
+# Maior que '>'
+
+# resultado 'True'
+print(2>1)
+# resultado 'False'
+print(2>3)
+
+# Menor que '<'
+
+# resultado 'True'
+print(1<2)
+# resultado 'False'
+print(2<1)
+
+# Maior ou igual '>='
+
+# resultado 'True'
+print(2>=2)
+# resultado 'False'
+print(2>=3)
+
+# Menor ou igual '<='
+
+# resultado 'True'
+print(1<=2)
+# resultado 'False'
+print(2<=1)
