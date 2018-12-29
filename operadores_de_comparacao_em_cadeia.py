9ef8b8b1b7c23fd7fc910a8d76695f85acd6ad87
Matheus Gusmão <matheus.lemos@ccc.ufcg.edu.br>
2018-12-29 16:34:34 -0200

    Adicionando aula sobre operadores de comparação em cadeia.

---
+++ operadores_de_comparacao_em_cadeia.py
@@ -0,0 +1,68 @@
+# Projeto em python 3 referente a aula 25 do Curso python da plataforma Udemy.
+
+# Projeto em python 3 referente a aula 24 do Curso python da plataforma Udemy.
+
+# Operadores
+
+# and : 'e'
+# or : 'ou'
+
+# Igualdade '=='
+
+# resultado 'True'
+print(1==1==1.0)
+# resultaddo 'False'
+print(1==2==1.0)
+
+# Diferença '!=,<>'
+
+# resultado 'True'
+print(1!=2!=3)
+# resultado 'False'
+print(1!=1.0!=1.1)
+# resultado 'True'
+print (2<>1<>3)
+# resultado 'False'
+print(2<>2<>2)
+
+# Maior que '>'
+
+# resultado 'True'
+print(3>2>1)
+# resultado 'False'
+print(1>2>3)
+
+# Menor que '<'
+
+# resultado 'True'
+print(1<2<3)
+# resultado 'False'
+print(1<1<4)
+
+# Maior ou igual '>='
+
+# resultado 'True'
+print(4>=3>=2)
+# resultado 'False'
+print(2>=1>=3)
+
+# Menor ou igual '<='
+
+# resultado 'True'
+print(1<=2<=3)
+# resultado 'False'
+print(1<=2<=1)
+
+# Usando o operador 'and', True se todas as expressões forem verdadeiras, False caso qualquer das expressões seja falsa
+
+# resultado 'True'
+print(2>1 and 3>1)
+# resultado 'False'
+print(1>2 and 2>1)
+
+# Usando o operador 'or', True caso uma das expressões seja verdadeira, False se todas forem falsas
+
+# resultado 'True'
+print(1==1 or 1>2)
+# resultado 'False'
+print(1!=1 or 2!=2)
