2a8c37b66ddeeae1734ee7831968434721c05243
Matheus Gusmão <matheus.lemos@ccc.ufcg.edu.br>
2019-01-03 11:14:35 -0200

    Adicionando aula sobre Range.

---
+++ range.py
@@ -0,0 +1,18 @@
+# Projeto em python 3 referente a aula 30 do Curso python da plataforma Udemy.
+
+# Gerando uma lista de 0 à 9 com a função 'Range' e exibindo-a em seguida
+a = range(0,10)
+print(list(a))
+
+# Exibindo o tipo do objeto 'range'
+print(type(a))
+
+# Gerando uma nova lista com parâmetros start/stop e exbindo-a em seguida
+start = 0
+stop = 20
+x = range(start,stop)
+print(list(x))
+
+# Gerando uma lista de números pares de 0 à 50, passo = 2
+y = range(0,51,2)
+print(list(y))
