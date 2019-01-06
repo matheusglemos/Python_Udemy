ac910dd11b3e06c6f5bfe35735f8391deedae61c
Matheus Gusmão <matheus.lemos@ccc.ufcg.edu.br>
2019-01-06 14:03:26 -0200

    Adicionando aula sobre métodos.

---
+++ Métodos e funções/Funcoes/metodos.py
@@ -0,0 +1,27 @@
+# Projeto em python 3 referente a aula 35 do Curso python da plataforma Udemy.
+
+# Cria uma simples lista
+l = [1,2,3,4,5]
+
+'''
+--- Métodos de uma lista ---
+
+* append
+* count
+* extend
+* insert
+* pop
+* remove
+* reverse
+* sort
+
+'''
+
+# Adiciona um elemento à lista usando o método 'append' e exibe-a em seguida
+l.append(6)
+# resultado '[1,2,3,4,5,6]'
+print(l)
+
+# Contando quantas vezes um elemento se repete na lista usando o método 'count' e em seguida mostra a quantidade de repetições
+# resultado '1'
+print(l.count(2))
